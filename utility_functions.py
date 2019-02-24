16f64b2e1e399fb1a28f87ffcfd198ec2f7f58a6
Kevin Koste <kevin.koste@yale.edu>
2019-02-23 20:29:00 -0500

    sinusoid fit fn working, cleaning now

--- utility_functions.py
+++ utility_functions.py
@@ -1,12 +1,55 @@
 import pandas as pd
 import numpy as np
 import scipy as sp
-
-import utility_functions as fn
+import scipy.optimize
 
 import matplotlib
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
+
+def fit_sin(tt, yy):
+    """
+    Fits sinusoid to the input time sequence
+    Return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"
+    """
+    tt = np.array(tt)
+    yy = np.array(yy)
+    ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+    Fyy = abs(np.fft.fft(yy))
+    guess_freq = abs(ff[np.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+    guess_amp = np.std(yy) * 2.**0.5
+    guess_offset = np.mean(yy)
+    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
+
+    def sinfunc(t, A, w, p, c):  return A * np.sin(w*t + p) + c
+    popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
+    A, w, p, c = popt
+    f = w/(2.*np.pi)
+    fitfunc = lambda t: A * np.sin(w*t + p) + c
+    return {"amp":A, "omega":w, "phase":p, "offset":c, "freq":f, "period":1./f, "fitfunc":fitfunc, "maxcov":np.max(pcov), "rawres": (guess,popt,pcov)}
+
+
+def sinusoid_impute(df,):
+    """
+    Fits a sinusoidal curve to time-series data
+    Return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"
+    """
+    tt = np.array(tt)
+    yy = np.array(yy)
+    ff = np.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+    Fyy = abs(np.fft.fft(yy))
+    guess_freq = abs(ff[np.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+    guess_amp = np.std(yy) * 2.**0.5
+    guess_offset = np.mean(yy)
+    guess = np.array([guess_amp, 2.*np.pi*guess_freq, 0., guess_offset])
+
+    def sinfunc(t, A, w, p, c):  return A * np.sin(w*t + p) + c
+    popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
+    A, w, p, c = popt
+    f = w/(2.*np.pi)
+    fitfunc = lambda t: A * np.sin(w*t + p) + c
+    return {"amp":A, "omega":w, "phase":p, "offset":c, "freq":f, "period":1./f, "fitfunc":fitfunc, "maxcov":np.max(pcov), "rawres": (guess,popt,pcov)}
+
 
 def add_hours_before(df_in,hours_before):
     """
