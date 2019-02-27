d468a296cc93a9a62946e9a83b8b35a4454c200d
Kevin Koste <kevin.koste@yale.edu>
2019-02-26 19:50:28 -0500

    implemented XGB model

--- utility_functions.py
+++ utility_functions.py
@@ -88,6 +88,28 @@ def add_hours_before(df_in,hours_before):
     return df
 
 
+def date_features(df, label=None):
+    """
+    Creates time series features from datetime index
+    """
+    df['date'] = df.index
+    df['hour'] = df['date'].dt.hour
+    df['dayofweek'] = df['date'].dt.dayofweek
+    df['quarter'] = df['date'].dt.quarter
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    df['dayofyear'] = df['date'].dt.dayofyear
+    df['dayofmonth'] = df['date'].dt.day
+    df['weekofyear'] = df['date'].dt.weekofyear
+    
+    X = df[['hour','dayofweek','quarter','month','year',
+           'dayofyear','dayofmonth','weekofyear']]
+    if label:
+        y = df[label]
+        return X, y
+    return X
+
+
 def plot_feature(df, column, start, stop, **keyword_parameters):
     """
     Plots all features for a snapshot between two indices
